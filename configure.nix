a168a5f59c4353402ae3eb7b4f4f628e9477a84a
Nikola <48845744+ImNotNikola@users.noreply.github.com>
2023-07-18 11:46:36 -0400

    Update configure.nix

--- configure.nix
+++ configure.nix
@@ -4,9 +4,13 @@
 
 { config, pkgs, ... }:
 
+let
+  impermanence = builtins.fetchTarball "https://github.com/nix-community/impermanence/archive/master.tar.gz";
+in
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      "${impermanence}/nixos.nix"
       ./hardware-configuration.nix
     ];
 
@@ -48,7 +52,93 @@
   #   useXkbConfig = true; # use xkbOptions in tty.
   # };
 
-  boot.supportedFilesystems = [ "btrfs" ];
+  # filesystems
+  fileSystems."/".options = [ "compress=zstd" "noatime" ];
+  fileSystems."/home".options = [ "compress=zstd" "noatime" ];
+  fileSystems."/nix".options = [ "compress=zstd" "noatime" ];
+  fileSystems."/persist".options = [ "compress=zstd" "noatime" ];
+  fileSystems."/persist".neededForBoot = true;
+
+  fileSystems."/var/log".options = [ "compress=zstd" "noatime" ];
+  fileSystems."/var/log".neededForBoot = true;
+
+  # reset / at each boot
+  boot.initrd = {
+    enable = true;
+    supportedFilesystems = [ "btrfs" ];
+
+    systemd.services.restore-root = {
+      description = "Rollback btrfs rootfs";
+      wantedBy = [ "initrd.target" ];
+      requires = [
+        "dev-vda3"
+      ];
+      after = [
+        "dev-vda3"
+        "systemd-cryptsetup@${hostname}.service"
+      ];
+      before = [ "sysroot.mount" ];
+      unitConfig.DefaultDependencies = "no";
+      serviceConfig.Type = "oneshot";
+      script = ''
+        mkdir -p /mnt
+
+        # We first mount the btrfs root to /mnt
+        # so we can manipulate btrfs subvolumes.
+        mount -o subvol=/ /dev/vda3 /mnt
+
+        # While we're tempted to just delete /root and create
+        # a new snapshot from /root-blank, /root is already
+        # populated at this point with a number of subvolumes,
+        # which makes `btrfs subvolume delete` fail.
+        # So, we remove them first.
+        #
+        # /root contains subvolumes:
+        # - /root/var/lib/portables
+        # - /root/var/lib/machines
+        #
+        # I suspect these are related to systemd-nspawn, but
+        # since I don't use it I'm not 100% sure.
+        # Anyhow, deleting these subvolumes hasn't resulted
+        # in any issues so far, except for fairly
+        # benign-looking errors from systemd-tmpfiles.
+        btrfs subvolume list -o /mnt/root |
+        cut -f9 -d' ' |
+        while read subvolume; do
+          echo "deleting /$subvolume subvolume..."
+          btrfs subvolume delete "/mnt/$subvolume"
+        done &&
+        echo "deleting /root subvolume..." &&
+        btrfs subvolume delete /mnt/root
+
+        echo "restoring blank /root subvolume..."
+        btrfs subvolume snapshot /mnt/root-blank /mnt/root
+
+        # Once we're done rolling back to a blank snapshot,
+        # we can unmount /mnt and continue on the boot process.
+        umount /mnt
+      '';
+    };
+  };
+
+  # configure impermanence
+  environment.persistence."/persist" = {
+    directories = [
+      "/etc/nixos"
+    ];
+    files = [
+      "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+    ];
+  };
+
+  security.sudo.extraConfig = ''
+    # rollback results in sudo lectures after each reboot
+    Defaults lecture = never
+  '';
 
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
@@ -60,7 +150,6 @@
   services.zfs.trim.enable = true;
 
   services.nfs.server.enable = true;
-
 
   boot.plymouth.enable = true;
 
@@ -126,12 +215,17 @@
  	user = "syncthing";
  	guiAddress = "0.0.0.0:8384";
   };
- 
- users.users.nikola = {
+
+  users.mutableUsers = false;
+  users.users.nikola = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
-  # openssh.authorizedKeys.keys = [ pubkey.nikola ]  
-    };
+
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWVNch9BcjkMqS/Xwep+GN4HwqyRIjr3Cuw7mHpqsKr nixos" ];
+
+    # passwordFile needs to be in a volume marked with `neededForBoot = true`
+    passwordFile = "/persist/passwords/nikola";
+  };
 
 
   # List packages installed in system profile. To search, run:
@@ -169,7 +263,19 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    allowSFTP = false; # Don't set this if you need sftp
+    challengeResponseAuthentication = false;
+    extraConfig = ''
+      AllowTcpForwarding yes
+      X11Forwarding no
+      AllowAgentForwarding no
+      AllowStreamLocalForwarding no
+      AuthenticationMethods publickey
+    '';
+  };
 
   # Open ports in the firewall.
   networking.firewall = {
@@ -184,6 +290,7 @@
   # networking.firewall.enable = false;
 
   nix.settings.sandbox = true;
+  system.copySystemConfiguration = true;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
