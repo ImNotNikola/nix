c2daac00df674487c9b2ee986c6b69c8d1efbe8d
Nikola <48845744+ImNotNikola@users.noreply.github.com>
2023-07-19 13:52:45 -0400

    Update diskSetupZFS.sh

--- diskSetupZFS.sh
+++ diskSetupZFS.sh
@@ -2,16 +2,6 @@ set -ex
 
 DISK=(/dev/sdX)
 
-PART_GPT="gpt"
-PART_EFI="efi"
-PART_ROOT="rpool"
-
-ZFS_ROOT="rpool"
-
-ZFS_ROOT_VOL="nixos"
-
-MNT=$(mktemp -d)
-
 set +x
 
 MAINCFG="/mnt/etc/nixos/configuration.nix"
@@ -20,54 +10,47 @@ HWCFG="/mnt/etc/nixos/hardware-configuration.nix"
 set -x
 
 sgdisk --zap-all ${DISK}
-
 parted --script --align=optimal  "${DISK}" -- \
 mklabel gpt \
-mkpart EFI 2MiB 1GiB \
-mkpart rpool 1GiB 100% \
+mkpart efi 0% 1GiB \
+mkpart root 1GiB 100% \
 set 1 esp on 
 
-# Wait for a bit to let udev catch up and generate /dev/disk/by-partlabel.
 sleep 3s
 partprobe "${DISK}"
 udevadm settle
 
-# Create the root pool
 zpool create \
 	-o ashift=12 \
 	-o autotrim=on \
 	-O acltype=posixacl \
-	-O compression=zstd \
+	-O compression=on \
 	-O dnodesize=auto \
         -O normalization=formD \
 	-O relatime=on \
 	-O xattr=sa \
 	-O mountpoint=none \
-	-R /mnt \
-	${ZFS_ROOT} ${ZFS_ROOT_VDEV} /dev/disk/by-partlabel/${PART_ROOT}*
+	root /dev/disk/by-partlabel/root
 
-zfs create -o mountpoint=legacy -o compression=lz4 ${ZFS_ROOT}/${ZFS_ROOT_VOL}
-zfs create ${ZFS_ROOT}/${ZFS_ROOT_VOL}/home
-zfs create -o atime=off ${ZFS_ROOT}/${ZFS_ROOT_VOL}/nix
-zfs create ${ZFS_ROOT}/${ZFS_ROOT_VOL}/root
-zfs create ${ZFS_ROOT}/${ZFS_ROOT_VOL}/usr
-zfs create ${ZFS_ROOT}/${ZFS_ROOT_VOL}/var
-zfs create ${ZFS_ROOT}/${ZFS_ROOT_VOL}containers/
-zfs create -o mountpoint=/boot ${ZFS_ROOT}/${ZFS_ROOT_VOL}/boot
+zfs create -o mountpoint=legacy -o compression=lz4 root/local
+zfs create -o mountpoint=legacy -o compression=lz4 root/user
+zfs create -o mountpoint=legacy -o compression=lz4 root/user/home
+zfs create -o mountpoint=legacy -o compression=lz4  root/local/nix
+zfs create -o mountpoint=legacy -o compression=lz4 root/system
+zfs create -o mountpoint=legacy -o compression=lz4 root/system/root
+zfs create -o mountpoint=legacy -o compression=lz4 root/containers
+zfs create -o mountpoint=/boot root/boot
 
-# Create, mount and populate the efi partitions
-mkfs.vfat -n EFI /dev/disk/by-partlabel/${PART_EFI}
-mkdir -p /mnt/boot/${PART_EFI}
-mount -t vfat /dev/disk/by-partlabel/${PART_EFI} /mnt/boot/${PART_EFI}
+mkfs.vfat -n EFI /dev/disk/by-partlabel/efi
+mkdir -p /mnt/boot/efi
+mount -t vfat /dev/disk/by-partlabel/efi /mnt/boot/efi
 
-# Make sure we won't trip over zpool.cache later
 mkdir -p /mnt/etc/zfs/
 rm -f /mnt/etc/zfs/zpool.cache
 touch /mnt/etc/zfs/zpool.cache
 chmod a-w /mnt/etc/zfs/zpool.cache
 chattr +i /mnt/etc/zfs/zpool.cache
 
-# Generate and edit configs
 nixos-generate-config --root /mnt
 
 cp /mnt/etc/nixos/configure.nix /mnt/etc/nixos/configure.nix.old
@@ -81,9 +64,8 @@ sed -i "${ADDNR}i"' \      neededForBoot = true;' ${HWCFG}
 ADDNR=$(awk '/^  fileSystems."\/boot" =$/ {print NR+3}' ${HWCFG})
 sed -i "${ADDNR}i"' \      neededForBoot = true;' ${HWCFG}
 
-sed -i "s|\"abcd1234\"|\"$(head -c4 /dev/urandom | od -A none -t x4| sed 's| ||g' || true)\"|g" ${MNT}/etc/nixos/configuration.nix
+sed -i "s|\"abcd1234\"|\"$(head -c4 /dev/urandom | od -A none -t x4| sed 's| ||g' || true)\"|g" ${MAINCFG}
 
-set +x
 echo "nixos-install --no-root-passwd --root /mnt"
 echo "umount -Rl /mnt"
 echo "zpool export -a"
