e963c16dd7838b05a5c1ae2565f1b0c1635a6cdb
Nikola <48845744+ImNotNikola@users.noreply.github.com>
2023-07-19 14:49:54 -0400

    Update diskSetupZFS.sh

--- diskSetupZFS.sh
+++ diskSetupZFS.sh
@@ -59,20 +59,4 @@ chattr +i /mnt/etc/zfs/zpool.cache
 
 nixos-generate-config --root /mnt
 
-cp /tmp/configureation.nix /mnt/etc/nixos/configuration.nix
-
-sed -i 's|fsType = "zfs";|fsType = "zfs"; options = [ "zfsutil" "X-mount.mkdir" ];|g' ${HWCFG}
-
-ADDNR=$(awk '/^  fileSystems."\/" =$/ {print NR+3}' ${HWCFG})
-sed -i "${ADDNR}i"' \      neededForBoot = true;' ${HWCFG}
-
-ADDNR=$(awk '/^  fileSystems."\/boot" =$/ {print NR+3}' ${HWCFG})
-sed -i "${ADDNR}i"' \      neededForBoot = true;' ${HWCFG}
-
-sed -i "s|\"abcd1234\"|\"$(head -c4 /dev/urandom | od -A none -t x4| sed 's| ||g' || true)\"|g" ${MAINCFG}
-
-echo "nixos-install --no-root-passwd --root /mnt"
-echo "umount -Rl /mnt"
-echo "zpool export -a"
-echo "swapoff -a"
-echo "reboot"
+echo "done!"
