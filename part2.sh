c4712992f2813b0be48bba57d097d837b6ed871b
Nikola <48845744+ImNotNikola@users.noreply.github.com>
2023-07-19 14:50:33 -0400

    Create part2.sh

---
+++ part2.sh
@@ -0,0 +1,15 @@
+sed -i 's|fsType = "zfs";|fsType = "zfs"; options = [ "zfsutil" "X-mount.mkdir" ];|g' ${HWCFG}
+
+ADDNR=$(awk '/^  fileSystems."\/" =$/ {print NR+3}' ${HWCFG})
+sed -i "${ADDNR}i"' \      neededForBoot = true;' ${HWCFG}
+
+ADDNR=$(awk '/^  fileSystems."\/boot" =$/ {print NR+3}' ${HWCFG})
+sed -i "${ADDNR}i"' \      neededForBoot = true;' ${HWCFG}
+
+sed -i "s|\"abcd1234\"|\"$(head -c4 /dev/urandom | od -A none -t x4| sed 's| ||g' || true)\"|g" ${MAINCFG}
+
+echo "nixos-install --no-root-passwd --root /mnt"
+echo "umount -Rl /mnt"
+echo "zpool export -a"
+echo "swapoff -a"
+echo "reboot"
