1b1ca160aa1bd619a0efe7cd96a175a42267e16a
Nikola <48845744+ImNotNikola@users.noreply.github.com>
2025-03-04 20:09:48 -0500

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "NixOS flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-index-database = {
+      url = "github:Mic92/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-colors.url = "github:misterio77/nix-colors";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    impermanence = {
+      url = "github:nix-community/impermanence";
+    };
+  };
+
+  outputs = {self, nixpkgs, home-manager,...}@inputs: 
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    nixosConfigurations.server = {
+        laptop = mkSystem ./laptop/configuration.nix;
+        server = mkSystem .server/configuration.nix;
+    };
+  };
+}
