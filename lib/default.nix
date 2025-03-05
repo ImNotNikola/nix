f5da3690dc0ccc7d5cae488877ad265b3bdccf31
Nikola <48845744+ImNotNikola@users.noreply.github.com>
2025-03-04 20:43:41 -0500

    Create default.nix

---
+++ lib/default.nix
@@ -0,0 +1,29 @@
+{inputs}: let
+  lib = (import ./default.nix) {inherit inputs;};
+  outputs = inputs.self.outputs;
+in rec {
+  pkgsFor = sys: inputs.nixpkgs.legacyPackages.${sys};
+
+  mkSystem = config:
+    inputs.nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        inherit inputs outputs myLib;
+      };
+      modules = [
+        config
+        outputs.nixosModules.default
+      ];
+    };
+
+  mkHome = sys: config:
+    inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = pkgsFor sys;
+      extraSpecialArgs = {
+        inherit inputs myLib outputs;
+      };
+      modules = [
+        config
+        outputs.homeManagerModules.default
+      ];
+  };
+}
