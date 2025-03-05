6313a68ce6c8b3fc118648cc4c8632ed3094d610
Nikola <48845744+ImNotNikola@users.noreply.github.com>
2025-03-04 20:20:51 -0500

    Update home.nix

--- hosts/server/home.nix
+++ hosts/server/home.nix
@@ -1,1 +1,21 @@
 
+{
+  inputs,
+  outputs,
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [outputs.homeManagerModules.default];
+
+  HomeManager = {
+    bundles.general.enable = true;
+  };
+
+  home = {
+    username = "nikola";
+    stateVersion = "24.11";
+
+    packages = with pkgs; [];
+  };
+}
